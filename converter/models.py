c7b5afbeafefacc0bc9fa6a7611edcc3309cc8ac
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-11 19:06:25 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# This simple model will now show up in your Accounting Expert Admin Portal
 class ExcelToTallyLog(models.Model):
     file_name = models.CharField(max_length=255)
-    # Fixed the typo: removed the extra 'auto_'
     converted_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default="Success")
 
