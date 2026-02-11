2ca97c0fb077c156eb9ff8e35e72cff19ec85716
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-11 17:47:09 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,13 @@
 # mysite/urls.py
 from django.contrib import admin
-from django.urls import path
-from converter import views
+from django.urls import path, include
 
-# Renaming the Admin Site
+# Renaming the Admin Site - Eye-catching titles
 admin.site.site_header = "Accounting Expert Admin Portal"
-admin.site.site_title = "Accounting Expert Admin Portal"
-admin.site.index_title = "Welcome to the Admin Portal"
+admin.site.site_title = "Accounting Expert Admin"
+admin.site.index_title = "Welcome to your Financial Dashboard"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('register/', views.register_view, name='register'),
+    # include your other app urls here
 ]
