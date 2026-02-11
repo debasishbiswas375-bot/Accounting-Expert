a671f875118a05a681395185a1192ae25709bf18
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-11 19:00:57 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
-# By leaving this file without a custom User class, we fix the E304 error.
-# This allows Django to use its built-in, secure User model for your Admin Portal.
-
+# This simple model will now show up in your Accounting Expert Admin Portal
 class ExcelToTallyLog(models.Model):
-    """
-    Optional: A simple model to track your file conversions.
-    This will show up in your 'Accounting Expert Admin Portal'.
-    """
     file_name = models.CharField(max_length=255)
-    converted_at = models.DateTimeField(auto_auto_now_add=True)
+    # Fixed the typo: removed the extra 'auto_'
+    converted_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50, default="Success")
 
     def __str__(self):
