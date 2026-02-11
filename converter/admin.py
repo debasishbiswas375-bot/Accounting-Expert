d38580b73c76e406bbdb7d6facfd98e96296614a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-11 19:04:13 +0530

    Update admin.py

--- converter/admin.py
+++ converter/admin.py
@@ -1,22 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, ConversionLog, Product, Subscription
+from .models import ExcelToTallyLog
 
-# Register the User model
-admin.site.register(User, UserAdmin)
-
-# Register the Logs
-@admin.register(ConversionLog)
-class LogAdmin(admin.ModelAdmin):  # <--- FIXED (Removed .site)
-    list_display = ('user', 'file_name', 'status', 'timestamp')
-    list_filter = ('status', 'timestamp')
-
-# Register the New Products & Prices
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):  # <--- FIXED (Removed .site)
-    list_display = ('name', 'price', 'is_active')
-    list_editable = ('price', 'is_active')
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):  # <--- FIXED (Removed .site)
-    list_display = ('user', 'product', 'start_date', 'is_active')
+@admin.register(ExcelToTallyLog)
+class ExcelToTallyLogAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'converted_at', 'status')
+    list_filter = ('status',)
+    search_fields = ('file_name',)
