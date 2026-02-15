966010f371c928b82872b1a4266f2830e3378f6a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 19:02:20 +0530

    Update views.py

--- converter/views.py
+++ converter/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Home page view
 def home(request):
-    return render(request, 'home.html')  # Looks for converter/templates/home.html
-
-# Convert page view
-def convert(request):
-    # Example: you can add your conversion logic later
-    if request.method == 'POST':
-        # handle form submission here
-        return HttpResponse("Conversion logic goes here")
-    return render(request, 'convert.html')  # create converter/templates/convert.html
+    return render(request, 'home.html')
