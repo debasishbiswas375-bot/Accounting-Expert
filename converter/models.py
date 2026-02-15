c17160f5d81c42a8d9226f29d03c7841dfe0844d
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 18:57:41 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -1,29 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
-class Plan(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    duration_days = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
+from django.db import models
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=15, blank=True, null=True)
-
-    country = models.CharField(max_length=100, blank=True, null=True)
-    state = models.CharField(max_length=100, blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
-
-    plan = models.ForeignKey(
-        Plan,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    # Add extra fields if needed
+    # phone_number = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.username
