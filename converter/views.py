e28e3c383e34fd3e35763da6cb383f2423975705
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 19:06:06 +0530

    Update views.py

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,4 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render
 
-urlpatterns = [
-    path('', views.home, name='home'),
-]
+def home(request):
+    return render(request, 'home.html')
