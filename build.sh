125a3cb224362593174c68c54ade78e42cb06c34
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 11:40:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations # Detects the new 'created_at' fields
 python manage.py migrate
 
-# Auto-create Superuser
+# Create Admin User
 export DJANGO_SUPERUSER_PASSWORD="admin123"
 python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
