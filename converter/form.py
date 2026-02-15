8fafd76c823a879370ec05b3132483c58046a81a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 18:30:20 +0530

    Create form.py

---
+++ converter/form.py
@@ -0,0 +1,8 @@
+# converter/forms.py
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
