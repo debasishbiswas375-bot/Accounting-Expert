7f573e020705ef26d04bbb2438ac54ce909159c0
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 19:01:14 +0530

    Update admin.py

--- converter/admin.py
+++ converter/admin.py
@@ -1,31 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Plan
+from .models import CustomUser
 
-
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    # If you added extra fields, add them here
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {
-            "fields": (
-                "phone",
-                "country",
-                "state",
-                "city",
-                "plan",
-            )
-        }),
+        # ('Extra Info', {'fields': ('phone_number',)}),
     )
-
-    list_display = (
-        "username",
-        "email",
-        "phone",
-        "country",
-        "city",
-        "plan",
-        "is_staff",
-    )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Plan)
