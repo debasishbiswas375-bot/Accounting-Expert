28e48a12c9545988ac8bc0f226884f59cc759cbf
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 20:45:17 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('converter.urls')),
+    path('', include('converter.urls')),  # Make sure converter/urls.py exists
 ]
