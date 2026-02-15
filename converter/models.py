0b53a42e07ec9c903b3d9a8ee8fc869198b9ed35
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 11:59:57 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -11,4 +11,4 @@ class Topic(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=100)
     is_pro = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True) # FIX: Required for admin panel
+    created_at = models.DateTimeField(auto_now_add=True) # FIX: Added for TopicAdmin
