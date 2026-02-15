5abd1c47f5495885083721d7dbc0579d78af612a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 17:06:54 +0530

    Update models.py

--- converter/models.py
+++ converter/models.py
@@ -1,11 +1,29 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-class Topic(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration_days = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    country = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
+    plan = models.ForeignKey(
+        Plan,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
