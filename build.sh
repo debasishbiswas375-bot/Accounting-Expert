063030d3243e85709644311c760b7e45d10759dd
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 10:53:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 # exit on error
 set -o errexit
 
+# 1. Install dependencies
 pip install -r requirements.txt
 
-# Run migrations automatically
+# 2. Collect static files for the admin panel
 python manage.py collectstatic --no-input
+
+# 3. Run database migrations to Neon
 python manage.py migrate
+
+# 4. Create your Admin user (Change the password below!)
+export DJANGO_SUPERUSER_PASSWORD="YourSecurePassword123"
+python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
