21b624901b695f8abb4e2cb4c6bfe3eaea7f819a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-15 11:14:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies (ensure "pip" is the first word)
 pip install -r requirements.txt
-
-# Prepare the site
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Create Admin (Change password below)
+# Auto-create Superuser
 export DJANGO_SUPERUSER_PASSWORD="admin123"
 python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
